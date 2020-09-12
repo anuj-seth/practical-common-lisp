49e6721514ed749c2cf49e8db174a3e1e1de08a8
Anuj Seth <anujseth76@gmail.com>
2020-09-12 06:09:54 +0530

    use csv parser

--- src/pcl/chapter_23/spam.clj
+++ src/pcl/chapter_23/spam.clj
@@ -1,5 +1,7 @@
 (ns pcl.chapter-23.spam
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.data.csv :as csv]))
+
 
 (defonce max-ham-score 0.4)
 (defonce min-spam-score 0.6)
@@ -8,8 +10,36 @@
 
 (defrecord WordFeature [word spam-count ham-count])
 
+(defn csv-data->map
+  [csv-data]
+  (map zipmap
+       (->> (first csv-data)
+            (map keyword)
+            repeat)
+       (rest csv-data)))
 
-(io/resource "emails.csv")
+(defn extract-words
+  [text]
+  
+  )
+
+(defn lazy-read-csv
+  [csv-file]
+  (let [in-file (io/reader csv-file)
+        csv-seq (csv/read-csv in-file)
+        lazy (fn lazy [wrapped]
+               (lazy-seq
+                (if-let [s (seq wrapped)]
+                  (cons (first s) (lazy (rest s)))
+                  (.close in-file))))]
+    (lazy csv-seq)))
+
+;;(take 5 (map :spam (csv-data->map (lazy-read-csv (io/resource "emails.csv")))))
+
+(with-open [r (io/reader (io/resource "emails.csv"))]
+  (doall 
+   (take 5 (csv-data->map (csv/read-csv r)))))
+
 (defn score
   [_]
   )
